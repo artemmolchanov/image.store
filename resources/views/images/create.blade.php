6b96eecea5890c2a8d29efe1713fef37e9ceb920
Artem Molchanov <a.molchanov@ltst.ru>
2018-01-22 11:38:29 +0600

    fix front

--- resources/views/images/create.blade.php
+++ resources/views/images/create.blade.php
@@ -18,14 +18,16 @@
                 <label for="image">Image</label><br>
                 <input type="file" name="images"  id="image">
             </div>
-
-            <p>
-                <select class="js-example-basic-multiple js-states form-control select2-hidden-accessible" name="tags[]"  multiple="multiple" data-select2-id="59" tabindex="-1" aria-hidden="true">
-                    @foreach($tags as $tag)
-                        <option value='{{ $tag->id }}'>{{ $tag->name }}</option>
-                    @endforeach
-                </select>
-            </p>
+            <div class="form-group">
+                <label>Tags</label><br>
+                    <p>
+                        <select class="js-example-basic-multiple js-states form-control select2-hidden-accessible" name="tags[]"  multiple="multiple" data-select2-id="59" tabindex="-1" aria-hidden="true">
+                            @foreach($tags as $tag)
+                                <option value='{{ $tag->id }}'>{{ $tag->name }}</option>
+                            @endforeach
+                        </select>
+                    </p>
+            </div>
 
             <div class="form-group">
                 <button type="submit" class="btn btn-default">Submit</button>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,9 +2,6 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
 
     <title>Image store</title>
 
@@ -12,10 +9,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
 
-
-    <!-- Custom styles for this template -->
-    <link href="css/album.css" rel="stylesheet">
-
+    <link rel="stylesheet" href="css/album.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css"  />
 </head>
 
