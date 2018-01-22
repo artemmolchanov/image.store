9605ebaac5bcd23d95049ee2eebf400ed8b80ad4
Artem Molchanov <a.molchanov@ltst.ru>
2018-01-22 23:50:56 +0600

    last last fix

--- resources/views/images/image.blade.php
+++ resources/views/images/image.blade.php
@@ -1,9 +1,16 @@
 <div class="col-md-4">
     <div class="card mb-4 box-shadow">
+
         <a href="/images/{{ $image->id }}">{{ $image->name }}</a></h2>
 
-    <p class="blog-post-meta">
-        {{ $image->created_at->toFormattedDateString() }}
+    <p class="blog-post-meta">Tags:
+        @if(count($image->tags))
+            @foreach($image->tags as $tag)
+                <a href="/images/tags/{{ $tag->name }}" >
+                    #{{ $tag->name }}
+                </a>
+            @endforeach
+        @endif
     </p>
         <img class="card-img-top" src="/storage/{{ $image->path }}" alt="Card image cap">
 
